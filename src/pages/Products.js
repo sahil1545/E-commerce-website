d6a251e9b6eba23635be1249ffbdf8d0dd1cc20a
sahil1545 <sahil979837@gmail.com>
2026-01-23 13:01:52 +0530

    resolving frontend issues

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef} from "react";
+import { useEffect, useState,} from "react";
 import api from "../services/api";
 import { useUI } from "../context/UiContext";
 import { useAuth } from "../context/Authcontext";
